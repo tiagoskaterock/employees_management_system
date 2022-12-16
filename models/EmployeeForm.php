b6dd860ae7daba5383d0c9052a2cc50403fe2923
Tiago Lemes <tiago.lemes@windel.com.br>
2022-12-16 09:47:24 -0300

    Employee Form

---
+++ models/EmployeeForm.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class EmployeeForm extends Model {
+
+	public $name;
+	public $job;
+	public $salary;
+
+	public function rules() {
+		return [
+			[ ['name', 'job', 'salary'], 'required'],
+			[ 'salary', 'number'],
+		]
+	}
+}
